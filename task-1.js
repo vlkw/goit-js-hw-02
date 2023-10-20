cbd2e13adfd23d98db951750892bf35545490643
vlkw <138374305+vlkw@users.noreply.github.com>
2023-10-20 16:15:44 +0300

    s

---
+++ task-1.js
@@ -0,0 +1,9 @@
+'use strict'
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = quantity * pricePerDroid;
+    if(totalPrice > customerCredits) {
+        return "Insufficient funds!";
+    } else {
+      return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    }
+}
